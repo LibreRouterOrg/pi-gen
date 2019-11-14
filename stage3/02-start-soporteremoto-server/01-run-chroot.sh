073f3ed3ca6444ba75d164d41467cae0c0ec419b
German Ferrero <ferrero.gf@gmail.com>
2019-11-14 16:16:48 -0300

    startup server script

---
+++ stage3/02-start-soporteremoto-server/01-run-chroot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+# fetch and install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+# load nvm into path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# install lts version. Automatically resolves architecture (armv7)
+nvm install --lts
+# Get soporteremoto code
+cd $HOME
+git clone https://github.com/LibreRouterOrg/soporteremoto-dashboard.git
+# Install forever to run server in a daemon process.
+npm install -g forever
+cd soporteremoto-dashboard/federated-server
+# Install dependencies
+yarn install
+# Run
+forever start run.js
