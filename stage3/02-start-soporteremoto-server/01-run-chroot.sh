98e215268ffa885e06e90b0f0cdd5eaf2ebf8101
German Ferrero <ferrero.gf@gmail.com>
2019-11-22 23:48:33 -0300

    new script to load soporteremoto

--- stage3/02-start-soporteremoto-server/01-run-chroot.sh
+++ stage3/02-start-soporteremoto-server/01-run-chroot.sh
@@ -1,18 +1,25 @@
 #!/bin/bash -e
-# fetch and install nvm
-cd $HOME
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
-# load nvm into path
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# install lts version. Automatically resolves architecture (armv7)
-nvm install --lts
-# Get soporteremoto code
-git clone https://github.com/LibreRouterOrg/soporteremoto-dashboard.git
-# Install forever to run server in a daemon process.
-npm install -g forever
-cd soporteremoto-dashboard/federated-server
-# Install dependencies
-npm install
-# Run
-forever start run.js
+NODE_VERSION=12.13.1
+sudo -i -u ${FIRST_USER_NAME} bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash'
+sudo NODE_VERSION=${NODE_VERSION} -i -u ${FIRST_USER_NAME} bash -c '[ -s $NVM_DIR/nvm.sh ] && \. $NVM_DIR/nvm.sh && nvm install $NODE_VERSION'
+sudo -i -u ${FIRST_USER_NAME} bash -c 'cd $HOME && git clone https://github.com/LibreRouterOrg/soporteremoto-dashboard.git'
+sudo -i -u ${FIRST_USER_NAME} bash -c '[ -s $NVM_DIR/nvm.sh ] && \. $NVM_DIR/nvm.sh && cd $HOME/soporteremoto-dashboard/federated-server && npm install'
+# Create a systemd service
+cat << EOF
+[Unit]
+Description="SoporteRemoto Federated Server"
+Wants=network-online.target
+After=network.target network-online.target
+
+[Service]
+Environment=NODE_VERSION=${NODE_VERSION}
+ExecStart=/home/${FIRST_USER_NAME}/.nvm/nvm-exec npm start run.js
+ExecStop=/home/${FIRST_USER_NAME}/.nvm/nvm-exec npm stop federated-server
+WorkingDirectory=/home/${FIRST_USER_NAME}/soporteremoto-dashboard/federated-server
+User=${FIRST_USER_NAME}
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable sr-server.service
